e4f36f3125afb77652f1f061f921ec7f8bcc2072
Tom Krok <91782249+trak2022z@users.noreply.github.com>
2023-03-20 06:29:38 +0000

    3

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 /*
- * CSE 154
- * Pokeball Exercise
- *
  * Implements the functionality of the Pokeball webpage to show a mystery
- * Pokemon when a button is clicked. Currently incomplete
+ * Pokemon when a button is clicked.
  */
 
 
@@ -29,31 +26,3 @@ function changeImage() {
 }
 
 
-//MouseEnter
-let test = document.getElementById("test");
-
-// This handler will be executed only once when the cursor
-// moves over the unordered list
-test.addEventListener("mouseenter", function( event ) {
-  // highlight the mouseenter target
-  event.target.style.color = "red";
-
-  // reset the color after a short delay
-  setTimeout(function() {
-    event.target.style.color = "";
-  }, 1000);
-}, false);
-
-
-//MouseOver
-// This handler will be executed every time the cursor
-// is moved over a different list item
-test.addEventListener("mouseover", function( event ) {
-  // highlight the mouseover target
-  event.target.style.color = "orange";
-
-  // reset the color after a short delay
-  setTimeout(function() {
-    event.target.style.color = "";
-  }, 1000);
-}, false);
